7f32980899e107985a9379b8c563326e8905a379
Owen Parker <owen.rparker@gmail.com>
2016-09-19 17:43:27 +0100

    The scrollbar on the conversation_box div in postman/templates/postman/view.html scrolls to the bottom on loading

--- static/js/custom_js.js
+++ static/js/custom_js.js
@@ -2,6 +2,9 @@ jQuery(document).ready(function($) {
     $(".clickable-row").click(function() {
         window.document.location = $(this).data("url");
     });
+
+// The scrollbar on the conversation_box div in postman/templates/postman/view.html scrolls to the bottom on loading
+    $('div.conversation_box').scrollTop($('div.conversation_box')[0].scrollHeight);
 });
 
 $(function() {
